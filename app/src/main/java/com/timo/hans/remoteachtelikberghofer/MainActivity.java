9f4309e3db5dcd9d48ef0150984c13139285c29c
Timo Achtelik <timo.achtelik@stud.h-da.de>
2017-11-29 20:57:50 +0100

    Added menu and Icons

--- app/src/main/java/com/timo/hans/remoteachtelikberghofer/MainActivity.java
+++ app/src/main/java/com/timo/hans/remoteachtelikberghofer/MainActivity.java
@@ -3,12 +3,19 @@ package com.timo.hans.remoteachtelikberghofer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     public MainActivity() {
     }
 
@@ -24,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
     }
-
     /** @brief wird beim Klicken auf Hello World ausgeführt.
      *
      * @param v
